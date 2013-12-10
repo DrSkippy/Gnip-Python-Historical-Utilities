7df1dd9126ad9e42b999648d93383708a4caddbb
DrSkippy27 <scott@drskippy.net>
2013-12-10 10:31:43 -0700

    Adding package support files

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='gnip-historical',
+    version='0.1.0',
+    author='Scott Hendrickson, Brian Lehman',
+    author_email='scott@drskippy.net',
+    packages=['gnip-historical'],
+    scripts=['creat_job.py', 'accept_job.py','reject_job.py', 'list_jobs.py'],
+    url='http://pypi.python.org/pypi/gnip-historical/',
+    license='LICENSE.txt',
+    description='Gnip Historical libarary and command scripts.',
+    long_description=open('README.txt').read(),
+)
